a212cfba805b002c3dc1c7d2b26e328bd2d25d53
AGoldian <goldianmail@yandex.ru>
2023-09-09 02:32:14 +0500

    additional functions

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,12 +2,19 @@ import json
 import os
 
 def class_identifier(path):
-    """Добавляет columns - 'n_cluster' где отображает номер кластера, относительно других.
-    path -- путь json-файла"""
+    """
+    Добавляет в ключ 'answers' для каждого элемента ключ - 'n_cluster' где отображает номер кластера, относительно других.
+    Также добавляет в ключ 'clusters' - количество кластеров. 
+    Args:
+        path: путь json-файла
+
+    Return:
+        dict: Словарь в формате json с добавленными ключами.
+    """
     
     with open(path, encoding='utf-8-sig') as f:
-        q = json.load(f)
-        answers = q['answers']
+        json_file = json.load(f)
+        answers = json_file['answers']
         clusters = {}
         num_cluster = 0
         
@@ -21,7 +28,35 @@ def class_identifier(path):
             
             batch['n_cluster'] = cl
         
-    return q
+        json_file['clusters'] = num_cluster
+        
+    return json_file
+
+
+def merge_duplicate_answers(json_data):
+    '''
+    Объединяет дублирующиеся ответы и суммирует их количество.
+    Args:
+        json_data type: dict[list[dict]]: Cловарь с ключом 'asnwers' и вложенными в нём списком словарей с ключами: 'answer' и 'count'.
+
+    Return:
+        list: Новый список словарей с уникальными записями 'answer' и их суммированными значениями 'count'.
+    '''
+    unique_answers = {}
+    
+    for item in json_data['answers']:
+        answer = item['answer']
+        count = item['count']
+        
+        if answer in unique_answers:
+            unique_answers[answer] += count
+        else:
+            unique_answers[answer] = count
+    
+    answers = [{'answer': answer, 'count': count} for answer, count in unique_answers.items()]
+    
+    return answers
+
 
 # ТРЕНИРОВОЧНЫЕ ДАННЫЕ
 output_file = r'data/train_data.json'
@@ -43,9 +78,12 @@ data = []
 
 for file in os.scandir('data/all/'):
     with open(file, encoding='utf-8-sig') as f:
-        q = json.load(f)
+        json_file = json.load(f)
+        json_file['answers'] = merge_duplicate_answers(json_file)
+
+        data.append(json_file)
     
-    data.append(q)
+    data.append(json_file)
 
 with open(output_file, encoding='utf-8-sig', mode='w') as f:
     f.write(json.dumps(data, indent=4))
